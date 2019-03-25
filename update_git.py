d74087e13c0db8a0501bcd19e09350b4305fc83c
Suruibin <suruibin@yahoo.com>
2019-03-25 20:29:59 +0800

    Update Code.

--- update_git.py
+++ update_git.py
@@ -11,16 +11,14 @@ import os
 def share_github():
     os.chdir("/home/suruibin/Code/")
     
-    os.system("cp /mnt/d/Code/Python/xsbdh/*.py /home/suruibin/Code/")
-    os.system("cp /mnt/d/Code/Python/xsbdh/*.ui /home/suruibin/Code/")
-    os.system("cp /mnt/d/Code/Python/xsbdh/*.txt /home/suruibin/Code/")
+    os.system("cp /mnt/d/Code/Python/xsbdh/*.7z /home/suruibin/Code/")
+
 
     os.system("/usr/bin/git add .")
     os.system("/usr/bin/git commit -m \"Update Code.\" -a >/tmp/msg.txt")
     os.system("/usr/bin/git push -q")
 
-    
-    os.chdir("/mnt/d/Software/TDX_SXBDH/T0002/signals/signals_user_4/")
+    print("已同步!")
 
 if __name__ == "__main__":
     share_github()
